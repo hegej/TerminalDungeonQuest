2df7c5dbc571455947b41caff014de52989bcf50
HegeJ <hege.jacobsen12@gmail.com>
2024-06-16 22:23:06 +0200

    Update BaseCharacterParameters.cs

--- DungeonGameLogic/Characters/CharacterParameters/BaseCharacterParameters.cs
+++ DungeonGameLogic/Characters/CharacterParameters/BaseCharacterParameters.cs
@@ -10,10 +10,14 @@ namespace DungeonGameLogic.Characters.CharacterParameters
         public int Health { get; set; }
         public int Strength { get; set; }
         public int Defense { get; set; }
-        public string SpecialAbility { get; set; }
+        public SpecialAbility SpecialAbility {get;set;}
         public int Speed { get; set; }
         public int Level { get; set; }
         public int Experience { get; set; }
         public int THAC0 { get; set; }
+    }
+
+    public class SpecialAbility(){
+        string name {get;set;}
     }
 }
